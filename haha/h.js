16f8040ae34cdf4faeb155871fcfedc8f27b2637
dai998812 <118254502+dai998812@users.noreply.github.com>
2022-11-15 20:57:40 +0700

    up

---
+++ haha/h.js
@@ -0,0 +1,22 @@
+function motrangweb()
+{
+    a = eval(bai11.txt.value);
+    c = bai11.btn.value;
+   if(a==1)
+   {
+        window.open("https://dai11332.github.io/NhaLinh/")
+   }
+   
+   if(a==2)
+   {
+    alert("huhu tại sao chứ ")
+   }
+   
+   else
+   if(a!=1 && a!=2)
+   {
+    alert("ban chưa nhap hoac nhap sai")
+   }
+    
+   
+}
